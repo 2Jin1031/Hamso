722ad2ed6f8481bea68f3db01ac2bddc827a8ca3
2jin1031 <2jin2.1031@gmail.com>
2023-09-30 01:20:51 +0900

    초기 백업 과정7

---
+++ src/main/java/halo/hamso/security/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+//package halo.hamso.security;
+//
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+//@EnableWebSecurity
+//public class SecurityConfiguration {
+//    private final JwtTokenizer jwtTokenizer;
+//    public SecurityConfiguration(JwtTokenProvider jwtTokenProvider) {
+//        this.jwtTokenizer = jwtTokenProvider;
+//    }
+//}
